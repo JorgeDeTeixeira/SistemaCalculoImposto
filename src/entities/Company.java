ecd2e812cb7b3ac7b83a5d6e5d6aa8179e71a54d
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-07-15 11:15:49 -0300

    Classe Company feita

---
+++ src/entities/Company.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class Company extends TaxPayer {
+
+	private int numberOfEmployees;
+
+	public Company() {
+		super();
+	}
+
+	public Company(String name, Double anualIncome, int numberOfEmployees) {
+		super(name, anualIncome);
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	public int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+	public void setNumberOfEmployees(int numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	@Override
+	public Double tax() {
+		if (getNumberOfEmployees() <= 10) {
+			return getAnualIncome() * 16;
+		} else {
+			return getAnualIncome() * 14;
+		}
+	}
+}
